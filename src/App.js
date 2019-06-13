1452f618dbcd5c603118d8d9dfd2a94996a251c5
Bryce Eklund <bryce.eklund@gmail.com>
2019-06-12 17:20:21 -0700

    finished reading for state and components checkpoint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Messages from './Messages';
+import TheDate from './state/TheDate';
+import Counter from './state/Counter/Counter';
 
 
 
@@ -10,6 +11,8 @@ function App() {
     <div className="App">
       <Messages name="Messages" unread={0} />
       <Messages name="Notifications" unread={10} />
+      <TheDate />
+      <Counter count={123} />
     </div>
   );
 }
--- src/Messages.js
+++ src/Messages.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Messages.css';
 
-export default function Messages(props) {
+function Messages(props) {
     const badge = props.unread
-    ? <div className="unread_count">{props.unread}</div> : null;
+    ? <div className="unread_count badge">{props.unread}</div> : null;
     return <div className="messages">{props.name} {badge}</div>
 }
+
+export default Messages;
